cc020ddbe41e610b56f4294f6a45d39a0e40ad2b
Zekihan <38537758+Zekihan@users.noreply.github.com>
2018-12-22 12:14:21 +0300

    Update Hospital.java

--- src/business/Hospital.java
+++ src/business/Hospital.java
@@ -1,3 +1,42 @@
 package business;
 
-public class Hospital {}
+import java.util.List;
+
+import business.doctor.Doctor;
+import business.patient.Patient;
+
+public class Hospital {
+	
+	private List<Doctor> doctors;
+	private List<Patient> patients;
+	
+	public Hospital(List<Doctor> doctors, List<Patient> patients) {
+	    setDoctors(doctors);
+	    setPatients(patients);
+	}
+	
+	public List<Doctor> getDoctors() {
+		return doctors;
+	}
+
+	public List<Patient> getPatients() {
+		return patients;
+	}
+
+	private void setDoctors(List<Doctor> doctors) {
+		this.doctors = doctors;
+	}
+
+	private void setPatients(List<Patient> patients) {
+		this.patients = patients;
+	}
+
+	public void addDoctor(Doctor doctor) {
+		doctors.add(doctor);
+	}
+	
+	public void addPatint(Patient patient) {
+		patients.add(patient);
+	}
+	
+}
